9c9d1200e6b12bd01d340fe5ad8ec6a6008684dc
xRose1 <ibedasee@gmail.com>
2019-02-06 22:16:49 -0500

    Initial Commit

---
+++ Instagram-Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram-Firebase
+//
+//  Created by Mac on 2/6/19.
+//  Copyright © 2019 Sbedx4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
