43d34db53b0174a94744fb7f4b0f98dc703a94d5
xRose1 <ibedasee@gmail.com>
2019-02-08 19:06:45 -0500

    First Version of Instagram-FireBase App

---
+++ Instagram-Firebase/AddPhoto/PhotoSelectorCell.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoSelectorCell.swift
+//  Instagram-Firebase
+//
+//  Created by Mac on 2/8/19.
+//  Copyright © 2019 Sbedx4. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorCell: UICollectionViewCell {
+    
+    let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ Instagram-Firebase/Utils/Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Extensions.swift
+//  Instagram-Firebase
+//
+//  Created by Mac on 2/6/19.
+//  Copyright © 2019 Sbedx4. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    //    func someRandomeMethod() {
+    //
+    //    }
+    
+}
